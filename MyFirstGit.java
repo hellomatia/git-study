61a99189fe6d587261b2ba6e59e37bf77d331a3c
hellomatia <shuaigejp80@gmail.com>
2023-08-18 14:23:47 +0900

    intellij를 통한 commit하기

--- MyFirstGit.java
+++ MyFirstGit.java
@@ -2,5 +2,11 @@ public class MyFirstGit {
     public static void main(String[] args) {
         System.out.println("Hello matia!");
         System.out.println("Bye matia...");
+
+        Bye bye = new Bye();
+        Hello hello = new Hello();
+
+        bye.printBye();
+        hello.printHello();
     }
 }
